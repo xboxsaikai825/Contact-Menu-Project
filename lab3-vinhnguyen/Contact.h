4de0370e4db0e59d433a89dc2455566b4bf86ad6
xboxsaikai825 <xboxs@DESKTOP-6V1AD7T>
2024-03-24 14:55:17 -0700

    +Contact 2

--- lab3-vinhnguyen/Contact.h
+++ lab3-vinhnguyen/Contact.h
@@ -9,6 +9,24 @@ public:
 	Contact();
 
 	Contact(string, string, string, string, string, string, DateTime, string, string, string, string, string, string, string, string, string);
+
+	/* Get methods
+	* methods to retrieve Contact attributes
+	*/
+	string get_id();
+
+	string get_role();
+
+	string get_company();
+
+	/* Set methods
+	* methods to change Contact attributes
+	*/
+	void set_id(string);
+
+	void set_role(string);
+
+	void set_company(string);
 private:
 	string id;
 	string role;
--- lab3-vinhnguyen/lab3-vinhnguyen.cpp
+++ lab3-vinhnguyen/lab3-vinhnguyen.cpp
@@ -5,6 +5,6 @@
 int main() {
 	DateTime time(10, 1, 32, 32, 32, 32);
 	Person test1("vinh", "thanh", "nguyen", time, "3245 santa", "union", "alameda", "california", "94587", "510737", "", "he/her", "xbox@gmail.com");
-	cout << test1;
+	test1.to_string();
 	return 0;
 }
